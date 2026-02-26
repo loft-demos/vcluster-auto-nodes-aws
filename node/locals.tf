9ffd0f0d820d7b1fa12c4857d2086f26183c56d7
Kurt Madel <kurt.madel@loft.sh>
2026-02-26 16:01:50 -0500

    Update locals.tf

--- node/locals.tf
+++ node/locals.tf
@@ -8,4 +8,18 @@ locals {
   user_data             = var.vcluster.userData != "" ? var.vcluster.userData : null
   instance_profile_name = nonsensitive(var.vcluster.nodeEnvironment.outputs.infrastructure["instance_profile_name"])
   cluster_tag           = nonsensitive(var.vcluster.nodeEnvironment.outputs.infrastructure["cluster_tag"])
+  
+  # All properties coming from the Loft NodeClaim
+  node_properties = try(var.vcluster.nodeClaim.spec.properties, {})
+
+  # Simple test
+  test_tag = try(local.node_properties["test"], null)
+
+  # Optional: allow passing many tags as JSON in a single property
+  aws_extra_tags = try(jsondecode(local.node_properties["awsTagsJson"]), {})
+
+  derived_aws_tags = merge(
+    local.aws_extra_tags,
+    local.test_tag != null ? { test = tostring(local.test_tag) } : {}
+  )
 }
